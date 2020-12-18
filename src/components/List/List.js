3d7db9efa61a04c9d74e9982988d30b782af778a
tothricsaj <ricktothdevelop@gmail.com>
2020-12-18 11:25:45 +0100

    basic listing todos

---
+++ src/components/List/List.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import style from './List.module.css'
+
+let todos = [
+  {
+    title: 'Foo',
+    finished: false,
+    expired: false,
+    dueToDate: ''
+  },
+  {
+    title: 'Bar',
+    finished: false,
+    expired: false,
+    dueToDate: ''
+  },
+
+  {
+    title: 'Baz',
+    finished: false,
+    expired: false,
+    dueToDate: ''
+  },
+
+  {
+    title: 'FooBarBaz',
+    finished: false,
+    expired: false,
+    dueToDate: ''
+  },
+
+]
+
+const TodoItem = props => {
+  return (
+    <li>
+      <input type="checkbox"/> {props.todoTitle}
+    </li>
+  )
+}
+
+const List = props => {
+
+  const todoItems = todos.map((el, i) => {
+    return <TodoItem key={i} todoTitle={el.title} />
+  })
+
+  return (
+    <div className={style.List}>
+      <ul>
+        {todoItems}
+      </ul>
+    </div>
+  )
+}
+
+export default List
